72fd526962f711ce19f36721d7c37a036b2456d3
Daniel Kuchař <daniel@laposa.ie>
2024-07-26 17:20:07 +0200

    remove service status and prototype folder

--- app/resources/views/dashboard/index.blade.php
+++ app/resources/views/dashboard/index.blade.php
@@ -21,14 +21,4 @@
         <a href="/sessions" class="button filled">View all sessions</a>
         <a href="/sessions/create" class="button filled">Create new session</a>
     </x-portal-section>
-    <x-portal-section title="Service status">
-        <ul>
-            <li>Authorised github ID: <a href="https://github.com/norbertlaposa">@norbertlaposa</a>
-            </li>
-            <li>Installed under organization: <a href="https://github.com/laposa">@laposa</a></li>
-            <li>Available repositories linked to portal: <strong class="online">23
-                    repositories</strong></li>
-        </ul>
-        <a href="/settings" class="button filled">Update settings</a>
-    </x-portal-section>
 </x-layout>
