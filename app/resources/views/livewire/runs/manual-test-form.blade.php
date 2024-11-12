d4b217e10d1e77dd2f9cc277582489c800f7c500
Daniel Kuchař <daniel@laposa.ie>
2024-11-12 12:14:34 +0100

    add first iteration of manual test styles

--- app/resources/views/livewire/runs/manual-test-form.blade.php
+++ app/resources/views/livewire/runs/manual-test-form.blade.php
@@ -2,40 +2,90 @@
     /** @var \App\Models\SessionServiceSuiteTest $test */
 @endphp
 
-<div>
-    {{ $test->getInstructions()->suite }}
-    {{ $test->getInstructions()->description }}
+<div class="manual-test">
+    @script
+    <script type="text/javascript">
 
-    <ol>
-        @foreach($test->getInstructions()->steps as $step)
-            <li>
-                <div>
-                    <strong>Action:</strong>
-                    @foreach($step->action as  $action)
-                        <div>{{ $action }}</div>
-                    @endforeach
-                </div>
-                @if (count($step->input) > 0)
-                    <div>
-                        <strong>Input:</strong>
-                        @foreach($step->input as $key => $input)
-                            <div>{{ $key }}: {{ $input }}</div>
+        document.addEventListener('click', function (e) {
+            let content = e.target.nextElementSibling;
+
+            if (e.target.classList.contains('expand-input') && !e.target.classList.contains('collapse-input')) {
+                content.style.maxHeight = content.scrollHeight + "px";
+                e.target.classList.add('collapse-input');
+                e.target.innerHTML = 'Hide input';
+            } else if (e.target.classList.contains('collapse-input')) {
+                content.style.maxHeight = null;
+                e.target.classList.remove('collapse-input');
+                e.target.innerHTML = 'Show input';
+            }
+        });
+
+    </script>
+    @endscript
+    <div class="heading">
+        <h2>{{ $test->getInstructions()->description }}</h2>
+        <div class="description">
+            {{ $test->getInstructions()->suite }}
+        </div>
+    </div>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Step</th>
+                <th>Action</th>
+                <th>Expected result</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($test->getInstructions()->steps as $index => $step)
+                <tr>
+                    <td>{{ $index + 1 }}</td>
+                    <td>
+                        @foreach($step->action as $action)
+                            {{ $action }}
                         @endforeach
-                    </div>
-                @endif
-                <div>
-                    <strong>Result:</strong>
-                    <ul>
-                        @foreach($step->result as $result)
-                            <li>
-                                {{ $result }}
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
-            </li>
-        @endforeach
-    </ol>
+
+                        @if($step->input)
+                            <div class="input">
+                                <span class="expand-input">Show input</span>
+                                <div class="content">
+                                    @foreach($step->input as $key => $input)
+                                        <!-- TODO copy to clipboard when attribute is added -->
+                                        <div><b>{{ $key }}</b>: {{ $input }}</div>
+                                    @endforeach
+                                </div>
+                            </div>
+                        @endif
+                    </td>
+                    <td>
+                        <ul class="bullet-list">
+                            @foreach($step->result as $result)
+                                <li>{{ $result }}</li>
+                            @endforeach
+                        </ul>
+                    </td>
+                    <td>
+                        <div class="radio-wrapper">
+                            <label for="test-step-{{$index}}-pass" class="custom-radio" title="Mark as Passed">
+                                <input type="radio" name="test-step-{{$index}}" id="test-step-{{$index}}-pass" />
+                                <div class="radio pass"></div>
+                                Pass
+                            </label>
+        
+                            <label for="test-step-{{$index}}-fail" class="custom-radio" title="Mark as Failed">
+                                <input type="radio" name="test-step-{{$index}}" id="test-step-{{$index}}-fail" />
+                                <div class="radio fail"></div>
+                                Fail
+                            </label>
+                        </div>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
     <form wire:submit="save">
         <fieldset>
             <legend>Test Status:</legend>
