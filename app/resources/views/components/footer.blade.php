0d9759a70040f6ce41efbd131c7c503edb674425
Norbert Laposa <norbert@laposa.ie>
2024-11-14 16:35:26 +0000

    rename test-portal to testflow

--- app/resources/views/components/footer.blade.php
+++ app/resources/views/components/footer.blade.php
@@ -1,3 +1,3 @@
 <footer>
     &copy;{{ now()->format('Y') }} Laposa Limited | <a
-        href="https://github.com/laposa/test-portal/issues">Report issue</a>
+        href="https://github.com/laposa/testflow/issues">Report issue</a>
