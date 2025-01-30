4de7d77dff5f6997b44daac1a311506e762f7a86
Norbert Laposa <norbert@laposa.ie>
2025-01-30 13:11:03 +0000

    display test suite / name properly

--- app/resources/views/livewire/runs/manual-test-form.blade.php
+++ app/resources/views/livewire/runs/manual-test-form.blade.php
@@ -3,20 +3,23 @@
     /** @var \App\Models\SessionServiceRun $run */
 @endphp
 @script
-<script type="text/javascript">
-    if('{{$result}}' != "") {
-        document.querySelector('button.submit').disabled = false;
-    }
-</script>
+    <script type="text/javascript">
+        if ('{{ $result }}' != "") {
+            document.querySelector('button.submit').disabled = false;
+        }
+    </script>
 @endscript
 <div class="manual-test">
     <div class="heading">
-        <h2>{{ $test->getInstructions()->description }}</h2>
+        <h2><span title="suite">{{ $test->getInstructions()->suite }}</span> /
+            <span title="test name">{{ $test->getInstructions()->name }}</span>
+        </h2>
         <div class="description">
-            {{ $test->getInstructions()->suite }}
-            @if($error != "")
+            <span title="description">{{ $test->getInstructions()->description }}</span>
+            @if ($error != '')
                 <div class="error">
-                    This test contains following errors which may negatively impact result of the run:
+                    This test contains following errors which may negatively impact result of the
+                    run:
                     <ul>{!! $error !!}</ul>
                 </div>
             @endif
@@ -33,13 +36,13 @@
             </tr>
         </thead>
         <tbody>
-            @foreach($test->getInstructions()->steps as $index => $step)
+            @foreach ($test->getInstructions()->steps as $index => $step)
                 <tr>
                     <td>{{ $index + 1 }}</td>
                     <td>
-                        @if(count($step->action) > 1)
+                        @if (count($step->action) > 1)
                             <ul class="bullet-list">
-                                @foreach($step->action as $action)
+                                @foreach ($step->action as $action)
                                     <li>{{ $action }}</li>
                                 @endforeach
                             </ul>
@@ -47,15 +50,19 @@
                             {{ $step->action[0] ?? '' }}
                         @endif
 
-                        @if($step->input)
+                        @if ($step->input)
                             <div class="input">
                                 <span class="expand-content"></span>
                                 <div class="expandable-content">
-                                    @foreach($step->input as $key => $input)
-                                        @if(is_array($input))
-                                            <div><b>{{ $input['description'] }}: </b> <span class="copy-to-clipboard" title="Copy to clipboard">{{ isset($input['value']) ? $input['value'] : '' }}</span></div>
+                                    @foreach ($step->input as $key => $input)
+                                        @if (is_array($input))
+                                            <div><b>{{ $input['description'] }}: </b> <span
+                                                    class="copy-to-clipboard"
+                                                    title="Copy to clipboard">{{ isset($input['value']) ? $input['value'] : '' }}</span>
+                                            </div>
                                         @else
-                                            <div><b>{{ $key }}</b>: {{ $input }}</div>
+                                            <div><b>{{ $key }}</b>: {{ $input }}
+                                            </div>
                                         @endif
                                     @endforeach
                                 </div>
@@ -64,21 +71,25 @@
                     </td>
                     <td>
                         <ul class="bullet-list">
-                            @foreach($step->result as $result)
+                            @foreach ($step->result as $result)
                                 <li>{{ $result }}</li>
                             @endforeach
                         </ul>
                     </td>
                     <td>
                         <div class="radio-wrapper">
-                            <label for="test-step-{{$index}}-pass" class="custom-radio" title="Mark as Passed">
-                                <input type="radio" name="test-step-{{$index}}" id="test-step-{{$index}}-pass" class="radio-pass" />
+                            <label for="test-step-{{ $index }}-pass" class="custom-radio"
+                                title="Mark as Passed">
+                                <input type="radio" name="test-step-{{ $index }}"
+                                    id="test-step-{{ $index }}-pass" class="radio-pass" />
                                 <div class="radio pass"></div>
                                 Pass
                             </label>
-        
-                            <label for="test-step-{{$index}}-fail" class="custom-radio" title="Mark as Failed">
-                                <input type="radio" name="test-step-{{$index}}" id="test-step-{{$index}}-fail" class="radio-fail"  />
+
+                            <label for="test-step-{{ $index }}-fail" class="custom-radio"
+                                title="Mark as Failed">
+                                <input type="radio" name="test-step-{{ $index }}"
+                                    id="test-step-{{ $index }}-fail" class="radio-fail" />
                                 <div class="radio fail"></div>
                                 Fail
                             </label>
@@ -90,39 +101,23 @@
     </table>
 
     <div>
-        <textarea
-            placeholder="Additional information"
-            id="comment"
-            name="comment"
-            wire:model="comment">
+        <textarea placeholder="Additional information" id="comment" name="comment" wire:model="comment">
         </textarea>
     </div>
 
     <div class="test-status">
         <div class="statuses">
             <label for="pass">
-                <input 
-                    type="radio"
-                    id="pass"
-                    name="result"
-                    value="pass"
-                    wire:model="result"
-                    required
-                />
+                <input type="radio" id="pass" name="result" value="pass"
+                    wire:model="result" required />
                 <div class="pass">
                     Pass
                 </div>
             </label>
-            
+
             <label for="fail">
-                <input
-                    type="radio"
-                    id="fail"
-                    value="fail"
-                    name="result"
-                    wire:model="result"
-                    required
-                />
+                <input type="radio" id="fail" value="fail" name="result"
+                    wire:model="result" required />
                 <div class="fail">
                     Fail
                 </div>
